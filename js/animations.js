9057f0d97a622fb7b67ba91e114430dc4efc73da
Shawn Chambless <shawn.chambless@gmail.com>
2015-05-29 15:45:49 -0600

    added animations.js

--- js/animations.js
+++ js/animations.js
@@ -3,13 +3,40 @@ $(function() {
     $('#tweet-submit').hide();
     $('#char-count').hide();
 
-
-    $('#tweet-content textarea').click(function(e) {
-        e.preventDefault();
+    $('#tweet-content textarea').focus(function() {
         $('#tweet-submit').show();
         $('#char-count').show();
+        $(this).height($(this).height() * 2);
 
     });
+
+    $('#tweet-content textarea').keydown(function() {
+        var remaining = 140 - $('#tweet-content textarea').val().length;
+        $('#char-count').html(remaining);
+
+        if (remaining <= 10) {
+            $('#char-count').css('color', '#d40d12');
+        }
+        else {
+            $('#char-count').css('color', 'inherit');
+        }
+
+        if (remaining > 0 && remaining < 140){
+            $('#tweet-controls button').prop('disabled', false);
+            $('#tweet-controls button').click(function() {
+                $('.tweet:first-child').prepend('<div class = "tweet"><div class = "content"><image class = "avatar" src = "img/damenleeturks.jpg"></image><strong class="fullname">My BFF</strong><span class="username">@mybff</span></div></div>');
+            });
+        }
+
+        else if (remaining <= 0) {
+            $('#tweet-controls button').prop('disabled', true);
+        }
+
+    });
+
+
+
+
 
 
 
